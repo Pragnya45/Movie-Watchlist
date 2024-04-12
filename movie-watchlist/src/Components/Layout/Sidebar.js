cff48667f828bf408488f24d58536216564b847d
Pragnya sahu <pragnya110@gmail.com>
2024-04-12 10:44:32 +0530

    sidebar changes

--- movie-watchlist/src/Components/Layout/Sidebar.js
+++ movie-watchlist/src/Components/Layout/Sidebar.js
@@ -3,6 +3,8 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { RiAccountCircleLine } from "react-icons/ri";
 import { useDispatch } from "react-redux";
+import { FaSearch } from "react-icons/fa";
+import { IoEllipsisVerticalSharp } from "react-icons/io5";
 
 export default function Sidebar() {
   const location = useLocation();
@@ -19,19 +21,27 @@ export default function Sidebar() {
 
   return (
     <aside
-      className={`h-full w-0 sm:w-64 absolute left-[0px]
-        sm:relative flex bg-white shadow-md  items-center flex-col gap-2 z-10`}
+      className={`h-full border-r-2 px-6 border-[#EAECF0] w-0 sm:w-72 absolute left-[0px]
+        sm:relative flex bg-white   items-center flex-col gap-2 z-10`}
     >
       <h1 className="mx-auto my-1 text-3xl font-semibold text-red-600">
         Watchlist
       </h1>
+      <div className="border-2 flex rounded-md  p-1 items-center border-[#EAECF0]">
+        <FaSearch color="#475467" size={20} />
+        <input
+          type="text"
+          placeholder="Search"
+          className="outline-none pl-2 w-full"
+        />
+      </div>
 
-      <div className="flex flex-col justify-between h-[77%] items-start w-full gap-3 pt-4">
+      <div className="flex flex-col justify-between h-[74%] items-start w-full gap-3 pt-4">
         {sidebarLinks.map((link, index) => (
           <NavLink
             key={index}
             to={link.path}
-            className={`w-full active:bg-red-600 ${
+            className={`w-full rounded-md active:bg-red-600 ${
               location.pathname === link.path
                 ? "bg-red-600 font-bold"
                 : "bg-tranparent"
@@ -41,24 +51,34 @@ export default function Sidebar() {
             `}
           >
             <button
-              className={`flex gap-2 w-full items-center  text-[1.05rem] text-black p-3 pl-6 hover:text-white hover:bg-red-600 ${
-                location.pathname === link.path ? "font-medium" : "font-normal"
+              className={`flex gap-2 w-full items-center  text-[1.05rem] text-black p-3  hover:text-white hover:bg-red-600 ${
+                location.pathname === link.path
+                  ? "font-medium text-white"
+                  : "font-normal"
               }`}
             >
-              {link.icon}
+              <MdOutlineHome
+                color={location.pathname === link.path ? "#fff" : "#000"}
+                size={25}
+              />
               {link.label}
             </button>
           </NavLink>
         ))}
-        <button
-          className="flex gap-2 w-full mt-auto items-center  text-[1.05rem] text-black p-3 pl-6 hover:bg-red-600 hover:text-white font-normal"
+        <div className="w-full h-[2px] bg-[#EAECF0]"></div>
+        <h3 className="my-1 text-xl font-semibold text-black">My lists</h3>
+        <div
+          className="flex gap-2 border-2 rounded-md border-[#EAECF0] w-full mt-auto items-center fill-black text-[1.05rem] text-black p-1 pl-2 hover:bg-red-600 hover:text-white hover:fill-white font-normal"
           onClick={() => {
             navigate("/signin");
           }}
         >
-          <RiAccountCircleLine className="fill-inherit" size={22} />
+          <RiAccountCircleLine className="fill-inherit" size={30} />
           Account
-        </button>
+          <button className="ml-auto cursor-pointer">
+            <IoEllipsisVerticalSharp className=" rotate-90" />
+          </button>
+        </div>
       </div>
     </aside>
   );
